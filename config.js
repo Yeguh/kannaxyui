7a1597089c1638d2797e14b7de4184f529884a23
Yeguh <110237186+Yeguh@users.noreply.github.com>
2022-08-04 15:51:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-// M A D E  B Y  Letta - Sama 🐰  
+// M A D E  B Y  Teguh
 // Credits, jangan dihapus atau diubah!
 
 // - - THANKS TO - -
 // • Allah SWT
-// • Nurutomo
+// • Teguh
 // • Bochilgaming
 // • Rominaru
 // • Kannachann
 // • The.sad.Boy01
 // • Rasel comel
 // • Xtreshe (Beban)
+// • Nurutomo
 // • Dll
-
 //[!] Jangan Dihapus, mending ditambahin 
 
 import { watchFile, unwatchFile } from 'fs'
@@ -50,31 +50,31 @@ global.sdc = 'https://discord.gg/WEJQjugTY7'
 global.snh = 'https://youtube.com/c/LeonGanz/'
 
 /*============== PAYMENT ==============*/
-global.pdana = '085608625102'
-global.povo = '085608625102'
-global.pgopay = '085608625102'
-global.ppulsa = '085608625102'
-global.ppulsa2 = '085608625102'
-global.psaweria = 'https://saweria.com/LeonGanz'
+global.pdana = '082286192918'
+global.povo = '082286192918'
+global.pgopay = '082286192918'
+global.ppulsa = '082286192918'
+global.ppulsa2 = '081270902325'
+global.psaweria = 'https://saweria.com/Teguh
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6282127104362'
-global.nomorown = '6282127104362'
-global.nomorpart = '6282127104362'
-global.namebot = 'Yusuf Oct'
-global.nameown = 'Yusuf Oct'
+global.nomorbot = '6281270902325'
+global.nomorown = '6282286192918'
+global.nomorpart = '6281270902325'
+global.namebot = '「 ᴇʟᴀɪɴᴀ 𝙱𝙾𝚃 汉  」'
+global.nameown = 'Teguh'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6282127104362', 'Yusuf Oct', true],
+  ['6282286192918', 'Teguh', true],
   ['6285608625102', 'Rama Agung', true],
   ['6282351319893', 'Muhimmah Putri', true],
   ['6283847909854', 'Zaky Rashif', true],
   ['6287877173955', 'Ferdiz-Afk', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = [] // Want some help?
+global.mods = [6282286192918] // Want some help?
 global.prems = [] // Premium user bukan disini nambahinnya, ketik .addprem @user 10
 
 
